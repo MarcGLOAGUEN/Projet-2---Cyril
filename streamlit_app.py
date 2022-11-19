0fdca22096dee449dd9a3db18846b964598ceba6
Marc GLOAGUEN <106138794+MarcGLOAGUEN@users.noreply.github.com>
2022-11-19 20:46:55 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -201,9 +201,10 @@ with tab2 :
 
     col_hist1, col_hist2 = st.columns([1,10])
     with col_hist2 :
-        st.image("st_espace_vide2.png", use_column_width=True)
+        
         if st.session_state["poster"]:
             for i in range(len(st.session_state["poster"]) - 7,-1, -7):
-                st.image(st.session_state["poster"][i:i+7], caption = st.session_state["title"][i:i+7], width=150)
                 st.image("st_espace_vide2.png", use_column_width=True)
+                st.image(st.session_state["poster"][i:i+7], caption = st.session_state["title"][i:i+7], width=150)
+             st.image("st_espace_vide2.png", use_column_width=True)   
 
